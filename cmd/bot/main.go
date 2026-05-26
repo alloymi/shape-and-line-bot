62087438ca945a530cf04db2affcbb933b84f89d
alloymi <alloymi@users.noreply.github.com>
2026-05-26 14:00:45 +0300

    fix: update responses

--- cmd/bot/main.go
+++ cmd/bot/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"SnLbot/internal/db"
 	"log"
 
 	"SnLbot/internal/bot"
@@ -12,19 +13,19 @@ func main() {
 	cfg := config.Load()
 	utils.InitLogger(cfg)
 
-	//dbConn, err := db.Connect(cfg)
-	//if err != nil {
-	//	log.Fatalf("DB connection error: %v", err)
-	//}
-	//defer dbConn.Close()
-	//
-	//err = db.RunMigrations(dbConn)
-	//if err != nil {
-	//	log.Fatalf("Migration error: %v", err)
-	//}
+	dbConn, err := db.Connect(cfg)
+	if err != nil {
+		log.Fatalf("DB connection error: %v", err)
+	}
+	defer dbConn.Close()
 
-	//tgBot, err := bot.NewBot(cfg, dbConn)
-	tgBot, err := bot.NewBot(cfg, nil)
+	err = db.RunMigrations(dbConn)
+	if err != nil {
+		log.Fatalf("Migration error: %v", err)
+	}
+
+	tgBot, err := bot.NewBot(cfg, dbConn)
+	//tgBot, err := bot.NewBot(cfg, nil)
 	if err != nil {
 		log.Fatalf("Bot init error: %v", err)
 	}
